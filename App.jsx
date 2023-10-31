3dc3479fd62c052653adf468f35106cb5a4150e5
GabriellDuarte <gabrielduartecoracine@gmail.com>
2023-10-31 09:00:46 -0300

    Tela Cadastro e tela listar

--- App.jsx
+++ App.jsx
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//npm install @react-navigation/native
+import { NavigationContainer } from '@react-navigation/native';
+//npm install @react-navigation/bottom-tabs
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import Cadastrar from './components/cadastrar';
+import Listar from './components/listar';
 
+const Tabs = createBottomTabNavigator();
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+return (
+        <NavigationContainer>
+            <Tabs.Navigator
+                screenOptions={{
+                    headerStyle:{ // Título: Plano de fundo cinza
+                        backgroundColor: '#CCC',            
+                    },
+                    headerTitleStyle:{ // Título: Cor e espessura da fonte, 
+                        color: "#000",
+                        fontWeight: '100',            
+                    },
+                    headerTitleAlign: 'center', // Título alinhado ao centro
+                }}
+            >
+                <Tabs.Screen 
+                    name='Cadastrar' 
+                    component={Cadastrar}
+                    options={{
+                        tabBarIcon:({color,size})=>(<Ionicons name="ios-add-circle-outline" size={size} color={color} />)
+                      }}
+                />
+                <Tabs.Screen 
+                    name='Listar' 
+                    component={Listar} 
+                    options={{
+                        tabBarIcon:({color,size})=>(<Ionicons name="ios-list-circle-outline" size={size} color={color} />)
+                    }}
+                />
+            </Tabs.Navigator>
+        </NavigationContainer>       
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
